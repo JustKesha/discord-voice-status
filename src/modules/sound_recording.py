896c1c2a6a7debb29d5aa57d8e141e980d8a393a
kesha <73757143+JustKesha@users.noreply.github.com>
2025-07-12 13:16:59 +0400

    Add defaults classes to record / recog modules

--- src/modules/sound_recording.py
+++ src/modules/sound_recording.py
@@ -1,7 +1,10 @@
 import speech_recognition as sr
 
+class Default:
+    RECORD_DURATION = 5.0 # Seconds
+
 def record_audio(
-        duration_sec: float = 5.0, 
+        duration_sec: float = Default.RECORD_DURATION, 
         source: sr.Microphone = sr.Microphone(),
         recognizer: sr.Recognizer = sr.Recognizer()
     ) -> sr.AudioData:
--- src/modules/speech_recognision.py
+++ src/modules/speech_recognision.py
@@ -1,9 +1,12 @@
 import speech_recognition as sr
 
+class Default:
+    ON_STT_ERROR = ""
+
 def sound_to_text(
         audio_data: sr.AudioData,
         method: sr.Recognizer = sr.Recognizer().recognize_google,
-        on_error: str = ""
+        on_error: str = Default.ON_STT_ERROR
     ) -> str:
     try:
         return method(audio_data)
