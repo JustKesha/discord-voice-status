a85fbe5eb199808f83fe01b93c61f7fce98d1eb8
kesha <73757143+JustKesha@users.noreply.github.com>
2025-07-12 10:53:57 +0400

    Update config module / Default class

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,23 +4,25 @@ from typing import Dict, Any
 import yaml
 from dotenv import load_dotenv
 
-DEFAULT_ROOT = Path(__file__).parent.parent.parent
-DEFAULT_YAML = "config.yaml"
-DEFAULT_ENVS = ".env"
-DEFAULT_ENVS_PREFIX = "PY_TDS_"
-DEFAULT_ENVS_SECTION = "env"
+class Default:
+    ROOT = Path(__file__).parent.parent.parent
+    YAML = "config.yaml"
+    ENVS = ".env"
+    ENVS_PREFIX = "PY_TDS_"
+    ENVS_SECTION = "env"
+    ENVS_REMOVE_PREFIX = True
 
 def load_yaml(
-        name: str = DEFAULT_YAML,
-        path: Path = DEFAULT_ROOT
+        name: str = Default.ROOT,
+        path: Path = Default.ROOT
         ) -> Dict[str, Any]:
     with open(path / name) as f:
         data = yaml.safe_load(f)
     return data
 
 def load_envs(
-        name: str = DEFAULT_ENVS,
-        path: Path = DEFAULT_ROOT
+        name: str = Default.ENVS,
+        path: Path = Default.ROOT
         ) -> os._Environ[str]:
     load_dotenv(path / name)
     return os.environ
@@ -28,9 +30,9 @@ def load_envs(
 def merge_envs_to_config(
         config: Dict[str, Any],
         envs: os._Environ[str],
-        prefix: str = DEFAULT_ENVS_PREFIX,
-        config_section: str = DEFAULT_ENVS_SECTION,
-        remove_prefix: bool = True
+        prefix: str = Default.ENVS_PREFIX,
+        config_section: str = Default.ENVS_SECTION,
+        remove_prefix: bool = Default.ENVS_REMOVE_PREFIX
         ) -> Dict[str, Any]:
     env_config = {
         (k[len(prefix):] if remove_prefix else k): v
@@ -43,17 +45,13 @@ def merge_envs_to_config(
         return {**config, **env_config}
 
 def load_config(
-        yaml_file: str = DEFAULT_YAML,
-        envs_file: str = DEFAULT_ENVS,
-        root_path: str = DEFAULT_ROOT, # NOTE Using same path for yaml & envs
-        envs_prefix: str = DEFAULT_ENVS_PREFIX,
-        envs_section: str = DEFAULT_ENVS_SECTION,
-        envs_remove_prefix: bool = True
+        yaml_file: str = Default.YAML,
+        envs_file: str = Default.ENVS,
+        root_path: str = Default.ROOT, # NOTE Using same path for yaml & envs
+        **kwargs
         ) -> Dict[str, Any]:
     return merge_envs_to_config(
         config = load_yaml(yaml_file, root_path),
         envs = load_envs(envs_file, root_path),
-        prefix = envs_prefix,
-        config_section = envs_section,
-        remove_prefix = envs_remove_prefix
+        **kwargs
         )
