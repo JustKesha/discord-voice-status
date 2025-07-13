b6e9dc5a3740857aca79939d6ed7231e3e012d9c
kesha <73757143+JustKesha@users.noreply.github.com>
2025-07-13 16:39:48 +0400

    Add main

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import json
+import modules
+import utils
+
+def main() -> int:
+    print("Starting...")
+
+    print("Loading config...")
+    try:
+        config = utils.load_config()
+        status = config["status"]
+    except FileNotFoundError:
+        print(f"Couldnt find the \"{utils.config.Default.YAML}\" file at \"{utils.config.Default.ROOT}\"")
+        return -1
+    print("Config file found")
+    print(f"Status: {status}")
+
+    print("Starting the main loop...")
+    result = loop(config)
+    print(f"Loop stopped with return code {result}")
+
+    return 1
+
+def loop(config: dict) -> int:
+    status: dict = config["status"]
+
+    print("Recording...")
+    try:
+        audio = modules.record_audio(
+            duration_sec = status["update_interval"]
+        )
+    except Exception as error:
+        print(f"Something went wrong: \"{error}\"")
+    # TODO Should instead always record in the background
+    print("Finished audio recording")
+
+    print("Running speech recognision...")
+    try:
+        text = modules.sound_to_text(audio)
+    except Exception as error:
+        print(f"Something went wrong: \"{error}\"")
+        return -1
+    print(f"Result: \"{text}\"")
+    
+    print("Requesting Discord API...")
+    response = modules.set_custom_status(text)
+    print(f"Discord responded with: {json.dumps(response, indent=2)}")
+
+    return 1
+
+if __name__ == "__main__":
+    print(f"Main returned {main()}")
